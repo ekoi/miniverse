09d57dfe1aa96fb1673f54179ee7d40650511404
root <root@tweb01.dans.knaw.nl>
2018-09-18 13:38:16 +0200

    wsgi module changed

--- miniverse/wsgi.py
+++ miniverse/wsgi.py
@@ -8,11 +8,18 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+sys.path.append('/var/www/miniverse.dans.knaw.nl/.virtualenvs/miniverse/lib/python2.7/site-packages')
+sys.path.append('/var/www/miniverse.dans.knaw.nl/.virtualenvs/miniverse')
+sys.path.append('/var/www/miniverse.dans.knaw.nl/miniverse')
+for k in sorted(os.environ.keys()):
+    v = os.environ[k]
+    print ('%-30s %s' % (k,v[:70]))
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miniverse.settings.heroku_dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miniverse.settings.local_settings")
+#heroku_dev")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
